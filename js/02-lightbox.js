82e305c155ba7f8e29096013a11aaa280b2c2301
TarasVarvarych <varvarychtaras@gmail.com>
2023-01-13 20:34:43 +0200

    123

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+function createGalleryMarkup(array) {
+  return array
+    .map(
+      ({
+        original,
+        preview,
+        description,
+      }) => `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`
+    )
+    .join("");
+}
+
+galleryEl.innerHTML = createGalleryMarkup(galleryItems);
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+galleryEl.addEventListener("click", onGalleryPictureClick);
+function onGalleryPictureClick(e) {
+  e.preventDefault();
+}
